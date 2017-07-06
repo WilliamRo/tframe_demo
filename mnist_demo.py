ac5a0c9f10363b7ce80555aa15f7b6c0968967d9
William <willi4m@zju.edu.cn>
2017-07-06 09:33:06 +0800

    ...

---
+++ mnist_demo.py
@@ -0,0 +1,18 @@
+import numpy as np
+import tensorflow as tf
+
+from tframe import console
+
+
+def main(_):
+  # Start
+  console.start("MNIST DEMO")
+
+  # ...
+
+  # End
+  console.end()
+
+
+if __name__ == "__main__":
+  tf.app.run()
